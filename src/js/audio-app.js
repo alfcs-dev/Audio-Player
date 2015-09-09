49d37bbe988f2b123818b57951e874fc3cc7835c
Alfredo Castañeda <alfcastaneda@gmail.com>
2015-09-09 03:14:09 -0500

    First working version without tests

--- src/js/audio-app.js
+++ src/js/audio-app.js
@@ -1,30 +1,43 @@
 (function() {
-  var app = angular.module('audioApp', ['ngAudio'])
-  .controller('FormCtrl', ['$scope', function ($scope) {
-  	$scope.audios = [{}]; 
-  	  $scope.addNewAudio = function() { 
-    	$scope.audios.push({}); 
-    	console.log('test');
-  	};
-  	$scope.addAudios = function(){
-  		console.log($scope);
-  		//alert(urls.length);
-  	};
-  	$scope.removeChoice = function() {
-	    var lastItem = $scope.audios.length-1;
-	    $scope.audios.splice(lastItem);
-  	};
+  var app = angular.module('audioApp', ['ngAudio', 'ngMaterial'])
+  .directive('audioPlayer', ['ngAudio', function(ngAudio){
+    return{
+      restrict: 'AE',
+      link: function(scope, attrs, elem){   
+        scope.audios = [{}];
+        scope.validUrls = [];
+        scope.validAudios = []; 
+        scope.addNewAudio = function() { 
+          scope.audios.push({}); 
+        };
+        scope.removeUrl= function(index) {
+          var lastItem = index;
+          scope.audios.splice(lastItem, 1);
+        };
+        scope.addAudios = function(isValid){
+          if(isValid){
+            scope.validUrls = scope.audios;
+            scope.indexTrack = 0;
+            scope.audio = ngAudio.load(scope.validUrls[0].url);
+          }
+        };
+        scope.next = function(audio, index){
+          scope.indexTrack += 1;
+          if(scope.indexTrack > (scope.validUrls.length - 1) ){
+            scope.indexTrack = 0;
+          } 
+          scope.audio = ngAudio.load(scope.validUrls[scope.indexTrack].url);
+        };
+        scope.prev = function(audio, index){
+          scope.index_track -= 1;
+          scope.audio.stop();
+          if(scope.indexTrack < 0){
+            scope.indexTrack = scope.validUrls.length - 1;
+          } 
+          scope.audio = ngAudio.load(scope.validUrls[scope.indexTrack].url);
+        };
+      },
+    };
+
   }]); 
-
- /* app.directive('audioInput', [ function () {
-	
-  	return {
-  		restrict: 'A',
-  		link: function (scope, iElement, iAttrs) {
-  			scope.audioUrl = "http://example.com/pathtomp3";
-  			console.log(scope);
-  		}
-  	};
-  }]);*/
-
 })();  
