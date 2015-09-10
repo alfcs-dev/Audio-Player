3b3d1e8940c7d77bea0d1df54b30988843b9560d
Alfredo Castañeda <alfcastaneda@gmail.com>
2015-09-10 01:01:39 -0500

    Updated version of main files completly working

--- src/js/audio-app.js
+++ src/js/audio-app.js
@@ -1,45 +1,42 @@
-  var app = angular.module('audioApp', ['ngAudio', 'ngMaterial', 'ngMock'])
-	.directive('audioPlayer', ['ngAudio',  function(ngAudio){
-	    return{
-	      restrict: 'AE',
-	      link: function(scope, attrs, elem){   
-	        scope.audios = [{}];
-	        scope.validUrls = [];
-	        scope.validAudios = []; 
-	        scope.addNewAudio = function() { 
-	          scope.audios.push({}); 
-	        };
-	        scope.removeUrl= function(index) {
-	          var lastItem = index;
-	          scope.audios.splice(lastItem, 1);
-	        };
-	        scope.addAudios = function(isValid){
-	          if(isValid){
-	            scope.validUrls = scope.audios;
-	            scope.objAudios = [];
-	          	angular.forEach(scope.validUrls, function(value, key){
-	          		scope.objAudios.push(ngAudio.load(value.url));
-	          	});
-	            scope.indexTrack = 0;
-	            scope.audio = scope.objAudios[0];
-	           	infoFile(scope.audio.id);
-	          } 
-	        };
-	        scope.next = function(index){
-	          scope.indexTrack += 1;
-	          if(scope.indexTrack > (scope.validUrls.length - 1) ){
-	            scope.indexTrack = 0;
-	          } 
-	         scope.audio = scope.objAudios[scope.indexTrack];
+var app = angular.module('audioApp', ['ngAudio', 'ngMaterial']);
 
+app.directive('audioList', ['ngAudio', function(ngAudio){
+	// Runs during compile
+	return {
+		restrict: 'AE', // E = Element, A = Attribute, C = Class, M = Comment
+		link: function($scope, iElm, iAttrs, controller) {
+			$scope.audios = [{}];
+			$scope.addNewAudio = function() { 
+	          $scope.audios.push({}); 
 	        };
-	        scope.prev = function(index){
-	          scope.indexTrack -= 1;
-	          if(scope.indexTrack < 0){
-	            scope.indexTrack = scope.validUrls.length - 1;
+	        $scope.removeUrl= function(index) {
+	          var lastItem = index;
+	          $scope.audios.splice(lastItem, 1);
+	        };
+	       	$scope.addAudios = function(isValid){
+	          if(isValid){
+	            $scope.objAudios = [];
+	          	angular.forEach($scope.audios, function(value, key){
+	          		$scope.objAudios.push(ngAudio.load(value.url));
+	          	});
+	            $scope.indexTrack = 0;
+	            $scope.audio = $scope.objAudios[0];
 	          } 
-	          scope.audio = scope.objAudios[scope.indexTrack];
 	        };
-	      },
-	    };
-	}]); 
+	       	$scope.next = function(index){
+				$scope.indexTrack += 1;
+				if($scope.indexTrack > ($scope.validUrls.length - 1) ){
+					$scope.indexTrack = 0;
+				} 
+	        	$scope.audio = scope.objAudios[scope.indexTrack];
+	        };
+	        $scope.prev = function(index){
+	          $scope.indexTrack -= 1;
+	          if($scope.indexTrack < 0){
+	            $scope.indexTrack = $scope.validUrls.length - 1;
+	          } 
+	          $scope.audio = $scope.objAudios[scope.indexTrack];
+	        };
+		}
+	};
+}]);
